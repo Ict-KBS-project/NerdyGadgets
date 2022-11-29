7feeb5c78f2594f57c7f486d675460178fe6d82b
whateveryeti26 <95095152+whateveryeti26@users.noreply.github.com>
2022-11-29 13:34:05 +0100

    Toevoegen WebPagina

---
+++ Nerdygadgets/cart.php
@@ -0,0 +1,145 @@
+<head>
+    <style type="text/css">
+        table, td, th
+        {
+            border:1px solid #666;
+        }
+        th
+        {
+            background-color:#666;
+            color:white;
+        }
+    </style>
+
+</head>
+
+<?php
+include "CartFuncties.php";
+include __DIR__ . "/header.php";
+?>
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <title>Winkelwagen</title>
+</head>
+<style type="text/css">
+    table {
+        width: 900px;
+        background-color: #656cf9;
+    }
+    table  ,tr{
+        border: 9px solid black;
+    }
+    table ,th{
+        border-bottom: 2px solid black !important;
+    }
+
+    .button {
+        background-color: #24298f;
+        text-transform: uppercase;
+        font-size: 0.8rem;
+        font-weight: 600;
+        display: block;
+        color: #FFFFFF;
+        width: 100%;
+        padding: 1rem;
+        border-radius: 0.25rem;
+        border: 0;
+        cursor: pointer;
+        outline: 0;}
+</style>
+<table class="cart" cellpadding="10" cellspacing="1"
+<body>
+<h1>Inhoud Winkelwagen</h1>
+
+<?php
+global $TotaalPrijs;
+$databaseConnection = connectToDatabase();
+
+if (isset($_POST["save"])) {
+    unset($_POST["save"]);
+    updateProductToCart($_POST["ItemID"], $_POST["aantal"]);
+}
+
+if (isset($_POST["Delete"])){
+    deleteProductFromCart($_POST["ItemID"]);
+}
+$cart = getCart();
+
+# Producten in $cart tonen + prijs
+foreach ($cart as $stockItemID => $aantal) {
+    $TotaalPrijs += $TotaalPrijs;
+
+    $NaamProduct = "";
+    $PrijsProduct = 0;
+
+    if ($stockItemID != NULL) {
+        $Query = PakQueryName($stockItemID);
+        $ToevoegenAanDatabase = mysqli_prepare($databaseConnection, $Query);
+        mysqli_stmt_execute($ToevoegenAanDatabase);
+        #query is toegevoegd aan database ^
+        # resultaat v
+        $Uitkomst = mysqli_stmt_get_result($ToevoegenAanDatabase);
+        $Uitkomst = mysqli_fetch_all($Uitkomst, MYSQLI_ASSOC);
+        #print_r($Uitkomst); //<-- test resultaat
+        foreach ($Uitkomst as $UitkomstID => $item) {
+            foreach ($item as $stockitem => $ItemNaam) {
+                $NaamProduct = $ItemNaam;
+            }
+        }
+
+        $Query = PakQueryPrijs($stockItemID);
+        $ToevoegenAanDatabase = mysqli_prepare($databaseConnection, $Query);
+        mysqli_stmt_execute($ToevoegenAanDatabase);
+        #query is toegevoegd aan database
+        # resultaat v
+        $Uitkomst = mysqli_stmt_get_result($ToevoegenAanDatabase);
+        $Uitkomst = mysqli_fetch_all($Uitkomst, MYSQLI_ASSOC);
+        #print_r($Uitkomst); //<-- test resultaat
+        foreach ($Uitkomst as $UitkomstID => $item) {
+            foreach ($item as $stockitem => $ItemPrijs) {
+                $PrijsProduct = $ItemPrijs;
+            }
+        }
+    }echo "<table>
+        <tr>
+        <th>naam</th>
+        <th>Aantal</th>
+        <th>prijs</th>
+        </tr>";
+
+    print("<tr>");
+    print("<td>".$NaamProduct."</td>");
+    ?>
+    <td>
+        <form action="Cart.php" method="post">
+            <input type="number" name="aantal"  value="<?php print($aantal);?>" min="0"/><br>
+            <input type="number"    id="Prijs"  name="ItemID"   value="<?php print($stockItemID);?>" readonly hidden>
+            <input type="submit" name="save" value="Opslaan">
+            <input type="submit" name="Delete" value="Delete">
+        </form>
+    </td>
+
+    <?php
+    $TotaalPrijsPerProduct = $aantal*$PrijsProduct;
+    $TotaalPrijs += $TotaalPrijsPerProduct;
+    print("<td>". round($TotaalPrijsPerProduct,2) ."</td>");
+    print("</tr>");
+    echo "</table>";
+    echo "</center>";
+}
+echo "<table>
+<tr>
+<th>
+Totaal Prijs:
+</th>
+</tr>
+";
+print("<tr>");
+print("<td>".round($TotaalPrijs,2)."</td>");
+print ("<a href='view.php?id0'>Naar artikelpagina van artikel 0")
+?>
+<a class="button" href="Betaalpagina.php">Betalen</a>
+</body>
+</html>
---
+++ Nerdygadgets/database.php
@@ -0,0 +1,262 @@
+<!-- dit bestand bevat alle code die verbinding maakt met de database -->
+<?php
+
+function connectToDatabase() {
+    $Connection = null;
+
+    mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT); // Set MySQLi to throw exceptions
+    try {
+        $Connection = mysqli_connect("localhost", "root", "", "nerdygadgets");
+        mysqli_set_charset($Connection, 'latin1');
+        $DatabaseAvailable = true;
+    } catch (mysqli_sql_exception $e) {
+        $DatabaseAvailable = false;
+    }
+    if (!$DatabaseAvailable) {
+        ?><h2>Website wordt op dit moment onderhouden.</h2><?php
+        die();
+    }
+
+    return $Connection;
+}
+
+function getHeaderStockGroups($databaseConnection) {
+    $Query = "
+                SELECT StockGroupID, StockGroupName, ImagePath
+                FROM stockgroups 
+                WHERE StockGroupID IN (
+                                        SELECT StockGroupID 
+                                        FROM stockitemstockgroups
+                                        ) AND ImagePath IS NOT NULL
+                ORDER BY StockGroupID ASC";
+    $Statement = mysqli_prepare($databaseConnection, $Query);
+    mysqli_stmt_execute($Statement);
+    $HeaderStockGroups = mysqli_stmt_get_result($Statement);
+    return $HeaderStockGroups;
+}
+
+function getStockGroups($databaseConnection) {
+    $Query = "
+            SELECT StockGroupID, StockGroupName, ImagePath
+            FROM stockgroups 
+            WHERE StockGroupID IN (
+                                    SELECT StockGroupID 
+                                    FROM stockitemstockgroups
+                                    ) AND ImagePath IS NOT NULL
+            ORDER BY StockGroupID ASC";
+    $Statement = mysqli_prepare($databaseConnection, $Query);
+    mysqli_stmt_execute($Statement);
+    $Result = mysqli_stmt_get_result($Statement);
+    $StockGroups = mysqli_fetch_all($Result, MYSQLI_ASSOC);
+    return $StockGroups;
+}
+
+function getStockItem($id, $databaseConnection) {
+    $Result = null;
+
+    $Query = " 
+           SELECT SI.StockItemID, 
+            (RecommendedRetailPrice*(1+(TaxRate/100))) AS SellPrice, 
+            StockItemName,
+            CONCAT('Voorraad: ',QuantityOnHand)AS QuantityOnHand,
+            SearchDetails, 
+            (CASE WHEN (RecommendedRetailPrice*(1+(TaxRate/100))) > 50 THEN 0 ELSE 6.95 END) AS SendCosts, MarketingComments, CustomFields, SI.Video,
+            (SELECT ImagePath FROM stockgroups JOIN stockitemstockgroups USING(StockGroupID) WHERE StockItemID = SI.StockItemID LIMIT 1) as BackupImagePath   
+            FROM stockitems SI 
+            JOIN stockitemholdings SIH USING(stockitemid)
+            JOIN stockitemstockgroups ON SI.StockItemID = stockitemstockgroups.StockItemID
+            JOIN stockgroups USING(StockGroupID)
+            WHERE SI.stockitemid = ?
+            GROUP BY StockItemID";
+
+    $Statement = mysqli_prepare($databaseConnection, $Query);
+    mysqli_stmt_bind_param($Statement, "i", $id);
+    mysqli_stmt_execute($Statement);
+    $ReturnableResult = mysqli_stmt_get_result($Statement);
+    if ($ReturnableResult && mysqli_num_rows($ReturnableResult) == 1) {
+        $Result = mysqli_fetch_all($ReturnableResult, MYSQLI_ASSOC)[0];
+    }
+
+    return $Result;
+}
+
+function getStockItemImage($id, $databaseConnection) {
+
+    $Query = "
+                SELECT ImagePath
+                FROM stockitemimages 
+                WHERE StockItemID = ?";
+
+    $Statement = mysqli_prepare($databaseConnection, $Query);
+    mysqli_stmt_bind_param($Statement, "i", $id);
+    mysqli_stmt_execute($Statement);
+    $R = mysqli_stmt_get_result($Statement);
+    $R = mysqli_fetch_all($R, MYSQLI_ASSOC);
+
+    return $R;
+}
+//functies voor informatie winkelmandje
+# pak naam van product
+function PakQueryName($stockItemID){ #naar databese.php
+    $Query = "
+        SELECT StockItemName
+        from stockitems
+        WHERE stockItemID = " . $stockItemID . " 
+        ";
+    return $Query;
+}
+# pak prijs van product
+function PakQueryPrijs($stockItemID){
+    $Query = "
+        SELECT (TaxRate*RecommendedRetailPrice*0.01+RecommendedRetailPrice) as VerkoopPrijs
+        from stockitems
+        WHERE stockItemID = " . $stockItemID . " 
+    ";
+    return $Query;
+}
+
+
+// initializing variables
+$username = "";
+$email = "";
+$errors = array();
+
+// connect to the database
+$db = mysqli_connect('localhost', 'root', '', 'nerdygadgets');
+
+// REGISTER USER
+if (isset($_POST['reg_user'])) {
+    // receive all input values from the form
+    $username = mysqli_real_escape_string($db, $_POST['username']);
+    $email = mysqli_real_escape_string($db, $_POST['email']);
+    $password_1 = mysqli_real_escape_string($db, $_POST['password_1']);
+    $password_2 = mysqli_real_escape_string($db, $_POST['password_2']);
+
+    // form validation: ensure that the form is correctly filled ...
+    // by adding (array_push()) corresponding error unto $errors array
+    if (empty($username)) {
+        array_push($errors, "Username is required");
+    }
+    if (empty($email)) {
+        array_push($errors, "Email is required");
+    }
+    if (empty($password_1)) {
+        array_push($errors, "Password is required");
+    }
+    if ($password_1 != $password_2) {
+        array_push($errors, "The two passwords do not match");
+    }
+
+    // first check the database to make sure
+    // a user does not already exist with the same username and/or email
+    $user_check_query = "SELECT * FROM users WHERE username='$username' OR email='$email' LIMIT 1";
+    $result = mysqli_query($db, $user_check_query);
+    $user = mysqli_fetch_assoc($result);
+
+    if ($user) { // if user exists
+        if ($user['username'] === $username) {
+            array_push($errors, "Username already exists");
+        }
+
+        if ($user['email'] === $email) {
+            array_push($errors, "email already exists");
+        }
+    }
+
+    // Finally, register user if there are no errors in the form
+    if (count($errors) == 0) {
+        $password = md5($password_1);//encrypt the password before saving in the database
+
+        $query = "INSERT INTO users (username, email, password) 
+  			  VALUES('$username', '$email', '$password')";
+        mysqli_query($db, $query);
+        $_SESSION['username'] = $username;
+        $_SESSION['success'] = "You are now logged in";
+        header('location: index.php');
+    }
+}
+
+// ...
+
+// LOGIN USER
+if (isset($_POST['login_user'])) {
+    $username = mysqli_real_escape_string($db, $_POST['username']);
+    $password = mysqli_real_escape_string($db, $_POST['password']);
+
+    if (empty($username)) {
+        array_push($errors, "Username is required");
+    }
+    if (empty($password)) {
+        array_push($errors, "Password is required");
+    }
+
+    if (count($errors) == 0) {
+        $password = md5($password);
+        $query = "SELECT * FROM users WHERE username='$username' AND password='$password'";
+        $results = mysqli_query($db, $query);
+        if (mysqli_num_rows($results) == 1) {
+            $_SESSION['username'] = $username;
+            $_SESSION['success'] = "You are now logged in";
+            header('location: index.php');
+        }else {
+            array_push($errors, "Wrong username/password combination");
+        }
+    }
+}
+// Bestel gegevens
+// initializing variables
+$customername = "";
+$lastname = "";
+$adres = "";
+$land = "";
+$postcode = "";
+$plaats = "";
+$provincie = "";
+$bank = "";
+$errors = array();
+
+if (isset($_POST['login_user'])) {
+    $customername = mysqli_real_escape_string($db, $_POST['customername']);
+    $lastname = mysqli_real_escape_string($db, $_POST['lastname']);
+    $adres = mysqli_real_escape_string($db, $_POST['adres']);
+    $land = mysqli_real_escape_string($db, $_POST['land']);
+    $postcode = mysqli_real_escape_string($db, $_POST['postcode']);
+    $plaats = mysqli_real_escape_string($db, $_POST['plaats']);
+    $provincie = mysqli_real_escape_string($db, $_POST['provincie']);
+    $bank = mysqli_real_escape_string($db, $_POST['bank']);
+
+    if (empty($customername)) {
+        array_push($errors, "Firstname is required");
+    }
+    if (empty($lastname)) {
+        array_push($errors, "lastname is required");
+    }
+    if (empty($adres)) {
+        array_push($errors, "Address is required");
+    }
+    if (empty($land)) {
+        array_push($errors, "Land is required");
+    }
+    if (empty($postcode)) {
+        array_push($errors, "Postcode is required");
+    }
+    if (empty($plaats)) {
+        array_push($errors, "Plaats is required");
+    }
+    if (empty($provincie)) {
+        array_push($errors, "Provincie is required");
+    }
+    if (empty($bank)) {
+        array_push($errors, "Bank is required");
+    }
+    if (count($errors) == 0) {
+        $query = "INSERT INTO bananen (customername, lastname, adres, land, postcode, plaats, provincie, bank) 
+                VALUES('$customername', '$lastname', '$adres', '$land', '$postcode', '$plaats', $provincie, '$bank')";
+        mysqli_query($db, $query);
+        if (mysqli_num_rows($results) == 1) {
+            $_SESSION['customername'] = $customername;
+            $_SESSION['success'] = "Bedankt";
+            header('location: bedankt%20bestellen.php');
+        }
+    }
+}
